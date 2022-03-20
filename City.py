b2da1c11a46eb3092c922a7c56c63118a09d4644
klone6ix <101984759+klone6ix@users.noreply.github.com>
2022-03-20 12:02:18 -0400

    Add files via upload

---
+++ City.py
@@ -0,0 +1,13 @@
+class City:
+    """Controls the city"""
+
+# City variables
+    city_stats = {
+        'name' : '',
+        'population' : 5,
+        'morale' : 100,
+        'army' : []
+        'buildings' : []
+        'employed' : 5,
+        
+    }
---
+++ cli_city.py
@@ -0,0 +1,8 @@
+import City
+
+active = True
+
+# Get city defualts
+
+# PUT WHILE HERE
+print('How would you like to start?')
